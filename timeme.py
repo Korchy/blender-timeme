9b467323532566339f6a8ec329ebc7f9feba94dd
Nikita Akimov <korchiy@yandex.ru>
2017-12-19 23:58:59 +0300

    update

--- timeme.py
+++ timeme.py
@@ -2,6 +2,7 @@
 # interplanety@interplanety.org
 
 import bpy
+import sys
 
 
 class TimeMe(bpy.types.Operator):
@@ -9,28 +10,84 @@ class TimeMe(bpy.types.Operator):
     bl_label = 'TimeMe: Start'
     bl_description = 'Start monitor my time'
 
+    cats = {}
+
     def modal(self, context, event):
-        print('modal')
-        print(event.type)
-        # return {'RUNNING_MODAL'}
+        if event.type in self.cats:
+            self.cats[event.type] += 1
+        else:
+            self.cats[event.type] = 1
+            newitem = bpy.context.scene.timeMeVars1.cats.add()
+            print(newitem.catname)
+            newitem.catname = event.type
+            # print('add ',event.type)
         return {'PASS_THROUGH'}
 
-    def invoke(self, context, event):
-        print('invoke')
-        # if context.object:
-        # context.window_manager.modal_handler_add(self)
-        # return {'RUNNING_MODAL'}
-        # return {'PASS_THROUGH'}
-
     def execute(self, context):
-        print('execute')
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
+
+    @staticmethod
+    def getCats(self, context):
+        itemlist = []
+        for i, key in enumerate(TimeMe.cats.keys()):
+            itemlist.append((key, key, '', TimeMe.cats[key]))
+        return itemlist
+
+
+class TimeMePrint(bpy.types.Operator):
+    bl_idname = 'timeme.print'
+    bl_label = 'TimeMe: Print'
+
+    def execute(self, context):
+        print(TimeMe.cats)
+        print('---')
+        for item in bpy.context.scene.timeMeVars1.cats:
+            print(item.catname, item.cattime)
+        return {'FINISHED'}
+
+
+class TimeMeVars(bpy.types.PropertyGroup):
+    cats = bpy.props.EnumProperty(
+        items = lambda self, context: TimeMe.getCats(self, context)
+    )
+
+
+def setCatsItemName(self, value):
+    self['catname'] = value
+
+
+class TimeMeCatsItem(bpy.types.PropertyGroup):
+    catname = bpy.props.StringProperty(
+        name="catname",
+        default="",
+        # set=lambda self, context, value: TimeMe.setCatsItemName(self, context, value)
+        set=setCatsItemName
+    )
+    cattime = bpy.props.StringProperty(name="cattime", default="")
+
+
+class TimeMeVars1(bpy.types.PropertyGroup):
+    # cats = bpy.props.CollectionProperty(type=TimeMeVars, name='cats')
+    cats = bpy.props.CollectionProperty(type=TimeMeCatsItem, name='cats')
+    activecat = bpy.props.IntProperty(name='activecat', default=-1)
 
 
 def register():
     bpy.utils.register_class(TimeMe)
+    bpy.utils.register_class(TimeMePrint)
+    bpy.utils.register_class(TimeMeCatsItem)
+    bpy.utils.register_class(TimeMeVars)
+    bpy.utils.register_class(TimeMeVars1)
+    bpy.types.Scene.timeMeVars = bpy.props.PointerProperty(type=TimeMeVars)
+    bpy.types.Scene.timeMeVars1 = bpy.props.PointerProperty(type=TimeMeVars1)
 
 
 def unregister():
+    del bpy.types.Scene.timeMeVars
+    del bpy.types.Scene.timeMeVars1
     bpy.utils.unregister_class(TimeMe)
+    bpy.utils.unregister_class(TimeMePrint)
+    bpy.utils.unregister_class(TimeMeCatsItem)
+    bpy.utils.unregister_class(TimeMeVars)
+    bpy.utils.unregister_class(TimeMeVars1)
--- timeme_panel.py
+++ timeme_panel.py
@@ -2,6 +2,14 @@
 # interplanety@interplanety.org
 
 import bpy
+from . import timeme
+
+
+class TimeMeCatsList(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        layout.label("", icon="COLOR")
+        layout.prop(item, "catname", '')
+        layout.prop(item, "cattime", '')
 
 
 class TimeMe_panel(bpy.types.Panel):
@@ -14,13 +22,19 @@ class TimeMe_panel(bpy.types.Panel):
     def draw(self, context):
         # self.layout.operator('uv_int.separate_meshloops', icon = 'UNLINKED', text = 'Blast')
         # self.layout.operator('uv_int.separate_meshloops_by_edge', icon = 'UNLINKED', text = 'Seam by edge')
-        # self.layout.operator('uv_int.weld_meshloops', icon = 'LINKED', text = 'Weld')
+        self.layout.prop(bpy.context.scene.timeMeVars, 'cats')
+
+        self.layout.template_list('TimeMeCatsList', 'Time Categories', bpy.context.scene.timeMeVars1, 'cats', bpy.context.scene.timeMeVars1, 'activecat', type='DEFAULT')
+
+        self.layout.operator('timeme.print', icon = 'LINKED', text = 'Print')
         pass
 
 
 def register():
     bpy.utils.register_class(TimeMe_panel)
+    bpy.utils.register_class(TimeMeCatsList)
 
 
 def unregister():
     bpy.utils.unregister_class(TimeMe_panel)
+    bpy.utils.unregister_class(TimeMeCatsList)
